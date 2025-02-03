e153a802e1915590593b3e68e1ea5ccbe2246def
Parkhi-lang <137688594+Parkhi-lang@users.noreply.github.com>
2025-02-03 22:28:23 +0530

    Update import.sql
    Changed ROUND upto 2 places

--- Week 3 Writing/meteorites/import.sql
+++ Week 3 Writing/meteorites/import.sql
@@ -18,9 +18,9 @@ UPDATE "meteorites_temp" SET "year" = NULL WHERE "year" = "";
 UPDATE "meteorites_temp" SET "lat" =  NULL WHERE "lat" = "";
 UPDATE "meteorites_temp" SET "long" = NULL WHERE "long" = "";
 
-UPDATE "meteorites_temp" SET "mass" = ROUND("mass",1);
-UPDATE "meteorites_temp" SET "lat" = ROUND("lat",1);
-UPDATE "meteorites_temp" SET "long" = ROUND("long",1);
+UPDATE "meteorites_temp" SET "mass" = ROUND("mass",2);
+UPDATE "meteorites_temp" SET "lat" = ROUND("lat",2);
+UPDATE "meteorites_temp" SET "long" = ROUND("long",2);
 
 DELETE FROM "meteorites_temp" WHERE "nametype" = "Relict";
 
